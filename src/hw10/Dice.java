4fe09af59f6bd6572091abae432d9ce941efaece
Robyn <Robyn@DESKTOP-Q53AP1R>
2016-12-10 19:33:31 -0800

    Week 10 ok Mike I did more for the for loop in dice.java so how do i get this go come out in hw10?

--- src/hw10/Dice.java
+++ src/hw10/Dice.java
@@ -5,46 +5,46 @@
  */
 package hw10;
 
-/**Extend the Dice class
-Keep track number of throws
-Keep track of history 
-Provide appropriate methods
-Rewrite Dice game to keep track of history in object.
+/**
+ * Extend the Dice class Keep track number of throws Keep track of history
+ * Provide appropriate methods Rewrite Dice game to keep track of history in
+ * object.
  *
  * @author Robyn
  */
 import java.util.*;
 import java.util.Random;
+
 public class Dice {
-       private static int lastRoll = 0;
-     static Random random = new Random();
-      private int die;
-     
-      //Throw() – return a random number between 1-6    
-	 //diceTotal() – Return current dice value  
-      //start with throwing one die
-       public int Throw()
-       { 
-         Random random = new Random();
-          die = random.nextInt(6)+1;
-          return die;
-       }//die  
-      
-       //throwing more than one die
-    public int Throw (int numberOfThrows)
-    {   
-        int roll=0;
-           int Throw=0;
-        for ( numberOfThrows=0;numberOfThrows<Throw;numberOfThrows++)
-        {
-        roll=roll+ (random.nextInt(6 ) + 1);        
+
+    private static int lastRoll = 0;
+    static Random random = new Random();
+    private int die;
+
+    //Throw() – return a random number between 1-6    
+    //diceTotal() – Return current dice value  
+    //start with throwing one die
+    public int Throw() {
+        Random random = new Random();
+        die = random.nextInt(6) + 1;
+        return die;
+    }//die  
+
+    //throwing more than one die
+    public int Throw(int numberOfThrows) {
+        int roll = 0;
+        int Throw = 0;
+        int diceTotal = 0;
+        for (numberOfThrows = 1; numberOfThrows < Throw; numberOfThrows++) {
+            roll = roll + (random.nextInt(6) + 1);
+            System.out.printf("Number is: %d%n ",numberOfThrows);
+            System.out.println();
         }
-   int diceTotal=0;
-   diceTotal=Throw();
-   return diceTotal;
-   
+
+        diceTotal = Throw();
+        return diceTotal;
+
     }//throw
 
-
 }//class
- 
+
